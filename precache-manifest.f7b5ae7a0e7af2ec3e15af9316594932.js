db69fe8c56349c973d9b388ff390beb7c4c47934
kagegrifon <kage.grifon@yandex.ru>
2019-06-30 09:44:30 +0500

    add build

---
+++ precache-manifest.f7b5ae7a0e7af2ec3e15af9316594932.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818ca8457aa832ccaac220d9c34de7a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35ad11bcb2762b514b24",
+    "url": "./static/css/main.bc2cdea6.chunk.css"
+  },
+  {
+    "revision": "91bbf5a2f2e353c2f845",
+    "url": "./static/js/2.2ec475fb.chunk.js"
+  },
+  {
+    "revision": "35ad11bcb2762b514b24",
+    "url": "./static/js/main.71273d02.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8dc90432f67e8a34ef44404180fc913e",
+    "url": "./static/media/cursor_icon_bucket.8dc90432.png"
+  },
+  {
+    "revision": "68e3a9bf294ea0866338ddb724a49558",
+    "url": "./static/media/cursor_icon_color_pick.68e3a9bf.png"
+  },
+  {
+    "revision": "7afd2891f406f635d6da3416f00d1b53",
+    "url": "./static/media/cursor_icon_eraser.7afd2891.png"
+  },
+  {
+    "revision": "7984cea0e12f5d002c411b3aa18e40b2",
+    "url": "./static/media/cursor_icon_pen.7984cea0.png"
+  },
+  {
+    "revision": "9e6c148590783855a2303f1bba144e7a",
+    "url": "./static/media/icon_bucket.9e6c1485.png"
+  },
+  {
+    "revision": "d3ae7f19c9bf01d13bfd5483aa179c06",
+    "url": "./static/media/icon_eraser.d3ae7f19.png"
+  },
+  {
+    "revision": "41a9461bbe91fef1dc6cb340a9db94b0",
+    "url": "./static/media/icon_pen.41a9461b.png"
+  }
+]);
